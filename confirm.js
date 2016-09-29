d238fd0318291dcae25bd9479fea770f4588168c
xenophy <xenophy@xenophy-no-iMac.local>
2016-09-29 18:56:34 +0900

    no message

--- confirm.js
+++ confirm.js
@@ -1,2 +1,27 @@
 console.log('confirm.js');
-　function unload(){}
+// 　function unload(){
+//     var pram=location.search;
+//     document.write("JavaScriptはONです。");
+//     document.write("<img src='../images/img002.gif'>");
+// document.close();
+// }
+
+// URLパラメータの取得
+var arg = new Object;
+var pair = location.search.substring(1).split('&');
+for (var i = 0; i < pair.length; i++) {
+  var kv = pair[i].split('=');
+  arg[kv[0]]=kv[1];
+}
+
+// HTMLにパラメータの値を埋め込み
+var name = arg.name,
+    tel = arg.tel;
+
+// 名前のテキストを変更
+// hoge.text = name;
+debugger;
+document.getElementById('username').innerText = name;
+
+console.log(name);
+console.log(tel);
--- input.js
+++ input.js
@@ -22,28 +22,28 @@ function btnClick(){
   // var url = baseUrl + '?email=' + email;
   // console.log(url);
 
-
+　//電話番号取得//
   var tel = document.getElementById('tel').value;
   // var baseUrl = 'http://localhost/confirm.html';
   // var url = baseUrl + '?tel=' + tel;
   // console.log(url);
-
+  //性別男か//
   var otoko = document.getElementById('otoko').checked;
   // var baseUrl = 'http://localhost/confirm.html';
   // var url = baseUrl + '?otoko=' + otoko;
   // console.log(url);
-
+  //性別女か//
   var onna = document.getElementById('onna').checked;
   // var baseUrl = 'http://localhost/confirm.html';
   // var url = baseUrl + '?onna=' + onna;
   // console.log(url);
-
+  //年齢１０代か//
   var juu = document.getElementById('juu').checked;
   // var baseUrl = 'http://localhost/confirm.html';
   // var url = baseUrl + '?juu=' + juu;
   // console.log(url);
 
-  var nijuu = document.getElementById('nijuu').checked;
+  var nijuu = document.getElementById('nijuu').chdecked;
   // var baseUrl = 'http://localhost/confirm.html';
   // var url = baseUrl + '?nijuu=' + nijuu;
   // console.log(url);
@@ -61,12 +61,11 @@ function btnClick(){
 
 
   //URLを生成する
-  // var sendUrl = "http://localhost/lesson1/confirm.html?name=name&address=address&email=email&tel=tel&otoko=otoko&onna=onna&juu=juu&nijuu=nijuu&sanjuu=sanjuu&yonjuu=yonjuu"
-  var sendUrl = baseUrl + "?name=" + name + "&address=" + address;
+  var sendUrl = baseUrl + "?name=" + name + "&address=" + address + "&email=" + email + "&tel=" + tel + "&otoko=" + otoko + "&onna=" + onna + "&juu=" + juu + "&nijuu=" + nijuu + "&sanjuu=" + sanjuu + "&yonjuu=" + yonjuu;
   console.log(sendUrl);
 
 
 
-  // location.href = sendUrl;
+  location.href = sendUrl;
 
 }
